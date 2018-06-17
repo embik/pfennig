c8a11100a7a1cf35ddec08bdbad497ceea2386e5
Marvin Beckers <mail@embik.me>
2018-06-17 10:52:06 +0200

    Move web ui into /web subrouter and apply CSRF to that

--- web/router.go
+++ web/router.go
@@ -3,6 +3,7 @@ package web
 import (
     "net/http"
 
+    "github.com/gorilla/csrf"
     "github.com/gorilla/mux"
 
     "github.com/embik/pfennig/web/router"
@@ -13,13 +14,18 @@ import (
 
 func NewRouter(assetPath string) *mux.Router {
     r := router.GetRouter()
+    r.HandleFunc("/", rootHandler).Methods("GET")
 
-    r.HandleFunc("/", requireLogin(handlers.GetIndexHandler)).Methods("GET").Name("index")
+    webRouter := r.PathPrefix("/web/").Subrouter()
 
-    r.HandleFunc("/login", handlers.GetLogin).Methods("GET").Name("login")
-    r.HandleFunc("/login", handlers.PostLogin).Methods("POST")
-    r.HandleFunc("/logout", requireLogin(handlers.GetLogout)).Methods("POST").Name("logout")
+    webRouter.HandleFunc("/", requireLogin(handlers.GetIndexHandler)).Methods("GET").Name("index")
+    webRouter.HandleFunc("/login", handlers.GetLogin).Methods("GET").Name("login")
+    webRouter.HandleFunc("/login", handlers.PostLogin).Methods("POST")
+    webRouter.HandleFunc("/logout", requireLogin(handlers.GetLogout)).Methods("POST").Name("logout")
 
+    csrfKey := "tee8MuT2uz5beeto7ohri9ush3aiwoh6"
+    CSRF := csrf.Protect([]byte(csrfKey), csrf.Secure(false))
+    webRouter.Use(CSRF)
 
     staticFileDirectory := http.Dir(assetPath)
     staticFileHandler := http.StripPrefix("/static/", http.FileServer(staticFileDirectory))
@@ -40,4 +46,8 @@ func requireLogin(next http.HandlerFunc) http.HandlerFunc {
 
         next.ServeHTTP(w, r)
     }
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, router.GetURI("index"), http.StatusTemporaryRedirect)
 }
