bb04409dfdf8afa3b6a39e7515d7777872dc6ade
Marvin Beckers <mail@embik.me>
2018-06-29 09:42:58 +0200

    Add functionality to get accounts belonging to a user

--- app/accounts.go
+++ app/accounts.go
@@ -5,11 +5,20 @@ import (
     "github.com/embik/pfennig/app/db_models"
 )
 
+func GetAccounts(userID uint) []models.Account {
+    var accounts []db_models.Account
+    var user db_models.User
+    getDB().First(&user, userID)
+    getDB().Model(&user).Related(&accounts, "Accounts")
+    getDB().Preload("Users").First(&accounts)
+
+    return convertAccounts(accounts)
+}
+
 func GetAccountTypes(userID uint) []models.AccountType {
     var account_types []db_models.AccountType
+    getDB().Where(&db_models.AccountType{UserID: int(userID)}).Or(&db_models.AccountType{IsGlobal: true}).Find(&account_types)
 
-    db := getDB()
-    db.Where(&db_models.AccountType{UserID: int(userID)}).Or(&db_models.AccountType{IsGlobal: true}).Find(&account_types)
     return convertAccountTypes(account_types)
 }
 
@@ -21,6 +30,24 @@ func CreateAccountType(data models.AccountType) bool {
     }).Error
 
     return err == nil
+}
+
+func convertAccounts(data []db_models.Account) []models.Account {
+    var accounts []models.Account
+    for _, e := range data {
+        var user_ids []uint
+        for _, user := range e.Users {
+            user_ids = append(user_ids, user.ID)
+        }
+        accounts = append(accounts, models.Account{
+            ID: e.ID,
+            Name: e.Name,
+            Bank: e.Bank,
+            AccountTypeID: e.AccountTypeID,
+            UserIDs: user_ids,
+        })
+    }
+    return accounts
 }
 
 func convertAccountTypes(data []db_models.AccountType) []models.AccountType {
